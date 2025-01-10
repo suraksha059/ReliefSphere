52e8f792f31637bf21c8e6e511f70e583b8cab2e
bibektimilsina00 <bibektimilsina444@gmail.com>
2025-01-10 13:30:13 +0545

    Initialize service locator in the init function

--- lib/init.dart
+++ lib/init.dart
@@ -1,1 +1,6 @@
-Future init() async {}
+import 'package:flutter_starter_kit/app/services/service_locator.dart';
+
+Future init() async {
+  ServiceLocator serviceLocator = ServiceLocator();
+  serviceLocator.initialize();
+}
