1e0803a1079a1ca8bad9bfc294196f881bed36a6
bibektimilsina00 <bibektimilsina444@gmail.com>
2025-01-10 13:45:33 +0545

    Update routing to set initial location to login screen and enable screen navigation

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,21 +1,24 @@
 import 'package:go_router/go_router.dart';
 
+import '../../presentation/screens/home_screen/home_screen.dart';
+import '../../presentation/screens/login_screen/login_screen.dart';
+
 final router = GoRouter(
   // Set the initial location
-  initialLocation: AppRoutes.homeScreen,
+  initialLocation: AppRoutes.loginScreen,
   routes: [
     GoRoute(
       path: AppRoutes.loginScreen,
       // Uncomment the following line to use the LoginScreen widget
-      // builder: (context, state) => LoginScreen(),
+      builder: (context, state) => LoginScreen(),
     ),
-    GoRoute(
-      path: AppRoutes.registerScreen,
-      // builder: (context, state) => RegisterScreen(),
-    ),
+    // GoRoute(
+    //   path: AppRoutes.registerScreen,
+    //   builder: (context, state) => RegisterScreen(),
+    // ),
     GoRoute(
       path: AppRoutes.homeScreen,
-      // builder: (context, state) => HomeScreen(),
+      builder: (context, state) => HomeScreen(),
     ),
   ],
 );
